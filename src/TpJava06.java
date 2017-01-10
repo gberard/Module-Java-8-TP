63a0d1e75c59928f02230ad54515c513e091cfbf
BERARD Guillaume <gberard@iocean.fr>
2017-01-10 18:50:09 +0100

    [TP-JAVA-06] Afficher la factorielle d'un nombre

---
+++ src/TpJava06.java
@@ -0,0 +1,10 @@
+class TpJava06 {
+	public static void main(String args[]){
+		int nb = Integer.parseInt(args[0]);
+		int fact = 1;
+		for(int i=0;i<nb;i++){
+			fact = fact * (i+1);
+		}
+		System.out.println(fact);
+	}
+}
