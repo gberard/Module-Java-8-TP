65118ec7c603d951c4b73aba45f62bff8d9240ac
BERARD Guillaume <gberard@iocean.fr>
2017-01-10 17:51:53 +0100

    [TP-JAVA-02] Conversion & Primitifs

---
+++ src/TpJava02.java
@@ -0,0 +1,15 @@
+class TpJava02 {
+	public static void main(String args[]){
+		System.out.println("Avec 1234");
+		int entier = 1234;
+		float decimal = entier;
+		System.out.println(entier);
+		System.out.println(decimal);
+		
+		System.out.println("Avec 123456789");
+		entier = 123456789;
+		decimal = entier;
+		System.out.println(entier);
+		System.out.println(decimal);
+	}
+}
