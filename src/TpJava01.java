6ada0c1bf631350848c8d7377234722624f7fcdc
BERARD Guillaume <gberard@iocean.fr>
2017-01-10 17:45:08 +0100

    [TP-JAVA-01-b] Arguments multiple de la ligne de commande

--- src/TpJava01.java
+++ src/TpJava01.java
@@ -1,5 +1,7 @@
 class TpJava01 {
 	public static void main(String args[]){
-		System.out.println("Bonjour " + args[0]);
+		for(int i=0;i<args.length;i++){
+			System.out.println("Bonjour " + args[i]);
+		}
 	}
 }
