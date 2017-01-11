ad9c6fa622b0bafbfcbcfe64196050a0508c8531
BERARD Guillaume <gberard@iocean.fr>
2017-01-11 10:41:24 +0100

    [TP-JAVA-12] Heritage.

--- src/com/dta/tp/Carre.java
+++ src/com/dta/tp/Carre.java
@@ -1,38 +1,13 @@
 package com.dta.tp;
 
-public class Carre {
-	
-	private Point basGauche, basDroit, hautGauche, hautDroit;
+public class Carre extends Rectangle{
 	
 	public Carre(Point p, int cote){
-		basGauche = new Point( p.getX() , p.getY() );
-		basDroit = new Point( p.getX() + cote, p.getY() );
-		hautGauche = new Point( p.getX() , p.getY() + cote );
-		hautDroit = new Point( p.getX() + cote , p.getY() + cote );
-	}
-	
-	public Point getPointBasGauche(){
-		return basGauche;
-	}
-	
-	public Point getPointBasDroit(){
-		return basDroit;
-	}
-	
-	public Point getPointHautGauche(){
-		return hautGauche;
-	}
-	
-	public Point getPointHautDroit(){
-		return hautDroit;
+		super(p, cote, cote);
 	}
 	
 	public String toString(){
 		return "[CARRE "+getPointBasDroit()+" "+getPointBasGauche()+" "+getPointHautGauche()+" "+getPointHautDroit()+" ]";
-	}
-	
-	public void affiche(){
-		System.out.println(this.toString());
 	}
 	
 }
